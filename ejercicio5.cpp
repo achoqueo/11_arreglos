1cccac63ed13ec67ffbf41ae0dccbbf0e0f7bbb5
achoqueo <achoqueo@unjbg.edu.pe>
2024-07-07 22:07:18 -0500

    Se pidio que se ingrese los nombres de los alumnos de la segunda clase

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -5,9 +5,11 @@ int main(){
     string FP[9999], PG[9999];
     cout<<"Ingrese la cantidad de alumnos que hay en la clase de fundamentos de programacion:";cin>>n;
     for(int i=1;i<=n;i++){
-    cout<<"Ingrese el nombre del estudiante "<<i<<" en el aula de fundamentos de programacion: "; cin>>FP[i];
+    cout<<"Ingrese el nombre del estudiante "<<i<<" en la clase de fundamentos de programacion: "; cin>>FP[i];
     }
     cout<<"Ingrese la cantidad de alumnos que hay en la clase programacion grafica: ";cin>>a;
-
+    for(int i=1;i<=a;i++){
+    cout<<"Ingrese el nombre del estudiante "<<i<<" en la clase de programacion grafica: "; cin>>PG[i];
+    }
 return 0;
 }
