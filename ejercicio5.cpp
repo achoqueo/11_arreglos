f17836422268ae44e11a183aae6b5ab10b930823
achoqueo <achoqueo@unjbg.edu.pe>
2024-07-07 22:02:01 -0500

    Se pidió que se ingrese la cantidad de alumnos en la clase de fundamentos de programación

---
+++ ejercicio5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n, a;
+    string FP[9999], PG[9999];
+    cout<<"Ingrese la cantidad de alumnos que hay en la clase de fundamentos de programacion";cin>>n;
+
+return 0;
+}
