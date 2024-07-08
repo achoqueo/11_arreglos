e2d94b3dbd99a326bbe755afb2fdef47313cc2cb
achoqueo <achoqueo@unjbg.edu.pe>
2024-07-07 19:40:11 -0500

    Se agregó un for para pedir que ingresen los numeros en orden, mientras estos mismo se agregan al arreglo creado

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int n,a, b, A[999];
+	int n,a, b, A[200];
 	a=0;
 	b=0;
     double Porcentaje,Promedio;
-    cout<<"Ingrese los numeros";cin>>n;
+    cout<<"Ingrese la cantidad de numeros: ";cin>>n;
+    for(int i=1;i<=n;i++){
+        cout<<"Ingrese el numero "<<i<<":";
+        cin>>A[i];
+        if(A[i]==0){
+            b++;
+        } else{
+            a=a+A[i];
+        }
+    }
 }
