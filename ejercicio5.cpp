8faacfb1a4b6a30cadc0feb7be0726b04dfc52f5
achoqueo <achoqueo@unjbg.edu.pe>
2024-07-07 22:04:14 -0500

    Se agrego un for para limitar al momento de pedir que ingresen los nombres de los alumnos en la priemra clase

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -4,6 +4,9 @@ int main(){
     int n, a;
     string FP[9999], PG[9999];
     cout<<"Ingrese la cantidad de alumnos que hay en la clase de fundamentos de programacion";cin>>n;
+    for(int i=1;i<=n;i++){
+    cout<<"Ingrese el nombre del estudiante "<<i<<" en el aula de fundamentos de programacion: "; cin>>FP[i];
+    }
 
 return 0;
 }
