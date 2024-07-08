711efc12c2ba2a3e714c9e4bfa28e20bdd909562
achoqueo <achoqueo@unjbg.edu.pe>
2024-07-07 22:05:41 -0500

    Se pide que se ingrese la cantidad de alumnos que hay en la clase de programación grafica

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -3,10 +3,11 @@ using namespace std;
 int main(){
     int n, a;
     string FP[9999], PG[9999];
-    cout<<"Ingrese la cantidad de alumnos que hay en la clase de fundamentos de programacion";cin>>n;
+    cout<<"Ingrese la cantidad de alumnos que hay en la clase de fundamentos de programacion:";cin>>n;
     for(int i=1;i<=n;i++){
     cout<<"Ingrese el nombre del estudiante "<<i<<" en el aula de fundamentos de programacion: "; cin>>FP[i];
     }
+    cout<<"Ingrese la cantidad de alumnos que hay en la clase programacion grafica: ";cin>>a;
 
 return 0;
 }
