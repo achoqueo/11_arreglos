3b6128d39283a19cc52b94123b525a5d5ffc336b
achoqueo <achoqueo@unjbg.edu.pe>
2024-07-07 20:41:09 -0500

    Declaración de los arreglos, e impresión de requerimiento para ingresar los numeros

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -5,3 +5,8 @@ int main() {
     int n;
     cout << "Ingrese el numero de elementos: ";
     cin >> n;
+    int arreglo[MAX];
+    int arreglo_par[MAX], arreglo_impar[MAX];
+    int numPar = 0, numImpar = 0;
+
+    cout << "Ingrese los numeros: ";
